62f8dc9535ffa17504993dd9c89aac2bedec432d
jamigardner <46544630+jamigardner@users.noreply.github.com>
2019-07-15 22:47:59 -0600

    Add files via upload

--- js/variables.js
+++ js/variables.js
@@ -1,17 +1,22 @@
 const navSlide = () => {
+    console.log("variable.js loaded");
     const burger = document.querySelector('.burger');
+    console.log(burger);
     const nav = document.querySelector('.nav-links');
+    console.log(nav);
     const navLinks = document.querySelectorAll('.nav-links li');
+    console.log(navLinks);
     
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-    
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
-        });
+    //Toggle Nav
+    //nav.classList.toggle('nav-hide');
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade ${index}s ease forwards`;
+        //link.style.animation = `navLinkFade 2s ease forwards ${index / 7 + 0}s`;
+        
     });
 }
+
 
 navSlide();
